5c2ae2cb12731de6f856755471eaf83d0a3920af
Mikhail Talapila <mikhailtalapila@yahoo.com>
2019-07-19 22:23:35 -0400

    Added the csv parser solution

---
+++ Salon.Console.CsvParser/CsvRow.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Salon.Console.CsvParser
+{
+    public class CsvRow: List<string>
+    {
+        public string LineText { get; set; }
+    }
+}
---
+++ Salon.Console.CsvParser/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Salon.Console.CsvParser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var reader = new StreamReader(@"Clients.csv"))
+            {
+                List<string> listOfThings = new List<string>();
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    while (!line.EndsWith('"'))
+                    {
+                        line += reader.ReadLine();
+                    }
+                    System.Console.WriteLine($"line {line}");
+                }
+            }
+        }
+    }
+}
